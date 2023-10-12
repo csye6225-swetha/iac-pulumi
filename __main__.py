d3110b022cc7d641bcabf5f8b6e2696e55093c23
swethapaturu9 <114616110+swethapaturu9@users.noreply.github.com>
2023-10-12 16:24:50 -0400

    final commit

--- __main__.py
+++ __main__.py
@@ -3,8 +3,13 @@ import pulumi_aws as aws
 from pulumi_aws import ec2
 
 
+config = pulumi.Config()
+
+vpc_cidr_block = config.require("vpc_cidr_block")
+
+
 vpc = ec2.Vpc("my_vpc",
-    cidr_block="172.16.0.0/16",
+    cidr_block=vpc_cidr_block,
     enable_dns_hostnames=True)
 
 
@@ -30,8 +35,14 @@ private_route_table = ec2.RouteTable("private_route_table",
 availability_zones = aws.get_availability_zones(state="available")
 
 for i, zone in enumerate(availability_zones.names[:3]):
+
+
+    public_subnet_cidr = f"{vpc_cidr_block.split('.')[0]}.{vpc_cidr_block.split('.')[1]}.{i}.0/24"
+    private_subnet_cidr = f"{vpc_cidr_block.split('.')[0]}.{vpc_cidr_block.split('.')[1]}.{i+3}.0/24"
+
+
     subnet = ec2.Subnet(f"public_subnet_{i}",
-        cidr_block=f"172.16.{i}.0/24",
+        cidr_block= public_subnet_cidr,
         vpc_id=vpc.id,
         map_public_ip_on_launch=True,
         tags={"Name": f"public-subnet-{i}"})
@@ -42,7 +53,7 @@ for i, zone in enumerate(availability_zones.names[:3]):
         route_table_id=public_route_table.id)
 
     subnet = ec2.Subnet(f"private_subnet_{i}",
-        cidr_block=f"172.16.{i+3}.0/24",
+        cidr_block=private_subnet_cidr,
         vpc_id=vpc.id,
         map_public_ip_on_launch=False,
         tags={"Name": f"private-subnet-{i}"})
