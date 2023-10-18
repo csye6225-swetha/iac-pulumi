470f353eaa94190403babb1d80e846d1da40f3ed
swethapaturu9 <114616110+swethapaturu9@users.noreply.github.com>
2023-10-18 18:11:40 -0400

    create ec2 and securitygroup

--- __main__.py
+++ __main__.py
@@ -30,7 +30,7 @@ public_route = ec2.Route("public_route",
 private_route_table = ec2.RouteTable("private_route_table",
     vpc_id=vpc.id)
 
-
+public_subnet_ids = []
 
 availability_zones = aws.get_availability_zones(state="available")
 
@@ -46,6 +46,8 @@ for i, zone in enumerate(availability_zones.names[:3]):
         vpc_id=vpc.id,
         map_public_ip_on_launch=True,
         tags={"Name": f"public-subnet-{i}"})
+    
+    public_subnet_ids.append(subnet.id)
 
     
     ec2.RouteTableAssociation(f"public_rta_{i}",
@@ -62,7 +64,70 @@ for i, zone in enumerate(availability_zones.names[:3]):
     ec2.RouteTableAssociation(f"private_rta_{i}",
         subnet_id=subnet.id,
         route_table_id=private_route_table.id)
+    
 
+application_security_group = ec2.SecurityGroup("applicationSecurityGroup",
+    description="Security group for web application EC2 instances",
+    vpc_id=vpc.id,
+    ingress=[
+      
+        ec2.SecurityGroupIngressArgs(
+            from_port=22,
+            to_port=22,
+            protocol="tcp",
+            cidr_blocks=["0.0.0.0/0"],
+        ),
+      
+        ec2.SecurityGroupIngressArgs(
+            from_port=80,
+            to_port=80,
+            protocol="tcp",
+            cidr_blocks=["0.0.0.0/0"],
+        ),
+       
+        ec2.SecurityGroupIngressArgs(
+            from_port=443,
+            to_port=443,
+            protocol="tcp",
+            cidr_blocks=["0.0.0.0/0"],
+        ),
+       
+        ec2.SecurityGroupIngressArgs(
+            from_port=8080,
+            to_port=8080,
+            protocol="tcp",
+            cidr_blocks=["0.0.0.0/0"],
+        ),
+    ],
+)
+
+
+key_pair_name = pulumi.Config().require("key_name")
+
+
+ami_id = pulumi.Config().require("ami_id")
+
+
+subnet_id_first = public_subnet_ids[0]
+
+
+ec2_instance = ec2.Instance("myEC2Instance",
+    ami=ami_id,
+    instance_type="t2.micro",
+    subnet_id=subnet_id_first,
+    security_groups=[application_security_group.id],  
+    root_block_device={
+        "volume_size": 25,
+        "volume_type": "gp2",  
+        "delete_on_termination": True,  
+    },
+    key_name=key_pair_name,
+    tags={
+        "Name": "MyEC2Instance",
+    })
+
+
+pulumi.export("ec2InstanceId", ec2_instance.id)
 pulumi.export("vpcId", vpc.id)
 pulumi.export("gatewayId", gateway.id)
 
